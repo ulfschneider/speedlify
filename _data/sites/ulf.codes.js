3a40f9ca8a180153dfdf1ab6929463c5412b3c7f
Ulf Schneider <ulfschneider@users.noreply.github.com>
2020-09-01 16:05:35 +0200

    Update ulf.codes.js

--- _data/sites/ulf.codes.js
+++ _data/sites/ulf.codes.js
@@ -1,10 +1,12 @@
 module.exports = {
 	name: "ulf.codes", // optional, falls back to object key
-	description: "The default sites that get tested",
+	description: "Ulf Schneider´s personal site",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://ulf.codes/"
+		"https://ulf.codes/",
+		"https://ulf.codes/blog/",
+		"https://ulf.codes/about/"
 	]
 };
